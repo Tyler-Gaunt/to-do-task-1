de2612e1408e681a9709bd010d64a826d11c492d
Tyler-Gaunt <158243239+Tyler-Gaunt@users.noreply.github.com>
2024-12-04 22:21:27 +1100

    final changes to the code before submitting.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, redirect, url_for, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from db import User, Todo
+from flask import session, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
@@ -10,6 +12,16 @@ app.secret_key = "supersecretkey"
 engine = create_engine('sqlite:///todo.db')
 Session = sessionmaker(bind=engine)
 db_session = Session()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SECRET_KEY'] = 'your_secret_key'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
 @app.route('/')
 def index():
@@ -25,23 +37,48 @@ def login():
 
         if user and check_password_hash(user.password, password):
             flash('Logged in successfully', 'info')
-
-            
-
-            
         else:
-            flash('Incorrect username or password', 'danger')
+            flash('Incorrect    sername or pa ssword', 'danger')
     return render_template('login.html')
-
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
 
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' not in session:
+        flash('Please log in to access this page', 'warning')
+        return redirect(url_for('login'))
 
-# 4 - create a dashboard.html page with a title
-# 5 - create a route for the dashboard that will return the dashboard.html page and test it!!
-# 6 - go to app.py and ask gpt to generate the code required to use the logged in user and grab the todos from the database
-# 7 - go back to the dashboard html aand ask it to display the todos in a nice format
+    user_id = session['user_id']
+    user = db_session.query(User).filter_by(id=user_id).first()
+    todos = db_session.query(Todo).filter_by(user_id=user_id).all()
+
+    return render_template('dashboard.html', user=user, todos=todos)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash('Username already exists. Please choose a different one.', 'danger')
+            return redirect(url_for('signup'))
+
+        hashed_password = generate_password_hash(password, method='scrypt')
+        new_user = User(username=username, password=hashed_password)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Signup successful! Please log in.', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('signup.html')
+
+
+
+
+# 7 - go back to the dashboard html and ask it to display the todos in a nice format
